fb470f48c256e41b1f059ccd0c522de1ec667dce
Pitos <biuro@gawsoft.pl>
2022-07-08 18:47:47 +0200

    Add lifecycle

--- create-s3.sh
+++ create-s3.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+
 # Set endpoint
 mc alias set s3 $ENDPOINT $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+
+lifecycle_days () {
+  # If set lifecycle
+  mc ilm import s3/$1 <<EOF
+  {
+      "Rules": [
+    {
+              "Expiration": {
+                  "Days": $2
+              },
+              "ID": "OldFiles",
+              "Filter": {
+                  "Prefix": "$3"
+              },
+              "Status": "Enabled"
+          }
+      ]
+  }
+  EOF
+}
 
 ## Create user
 mc admin user add s3 $access_key $secret_key
@@ -15,7 +36,15 @@ for i in {1..1000}; do
   if [ ${#value} -gt 0 ]; then
     echo "Creating bucket ${value}"
     mc mb s3/${value}
+
+    if [ ${#BUCKET_LIFECYCLE_DAYS} ];
+    then
+      lifecycle_days ${value} ${BUCKET_LIFECYCLE_DAYS} ${BUCKET_LIFECYCLE_PREFIX} 
+    fi
+    
   fi
 done
+
+
 
 exit 0
