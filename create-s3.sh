4024386bb5c6f2c1ec22d1e04e836b41e9945cd6
Pitos <biuro@gawsoft.pl>
2022-08-02 12:05:02 +0200

    Fix error with api

--- create-s3.sh
+++ create-s3.sh
@@ -4,7 +4,7 @@
 mc alias set s3 $ENDPOINT $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 lifecycle_days () {
-  # If set lifecycle
+# If set lifecycle
 
 mc ilm import s3/$1 <<EOF
   {
@@ -22,11 +22,18 @@ mc ilm import s3/$1 <<EOF
       ]
   }
 EOF
+
 }
 
 
 ## Create user
 mc admin user add s3 $access_key $secret_key
+
+if [ $? -gt 0 ];
+then
+  echo "Error with create account"
+  exit 1
+fi
 
 # Set Policy readwrite
 mc admin policy set s3 $POLICY_NAME user=$access_key
@@ -38,6 +45,12 @@ for i in {1..1000}; do
   if [ ${#value} -gt 0 ]; then
     echo "Creating bucket ${value}"
     mc mb s3/${value}
+
+    if [ $? -gt 0 ];
+    then
+      echo "Error with create bucket"
+      exit 1
+    fi
 
     if [ ${#BUCKET_LIFECYCLE_DAYS} -gt 0 ];
     then
